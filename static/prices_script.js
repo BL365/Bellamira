a262296d1be1f4a6815a7856c316dad78abd94bc
BL365 <bl365@bk.ru>
2017-02-06 21:51:24 +0300

    не работает событие по кнопке в форме с заисью мероприятия, в базу пишется все подряд, ошибка итератора в 162 стр hall.py(обращеие к лок. ST до присвоения

--- static/prices_script.js
+++ static/prices_script.js
@@ -1,12 +1,9 @@
-
-var str1 = document.getElementById('st_t').value
-var str2 = document.getElementById('end_t').value
-var str = str1 + "/" + str2;
-    A = function() {
-        jQuery.getJSON(
-        "/prices/str/$hall.id/",
+A = function(hall) {
+    var str = "/prices/" + document.getElementById('st_t').value + "/" + document.getElementById('end_t').value + "/" + hall + "/"
+    jQuery.getJSON(
+        str,
         function(json) {
                 return (json.result);
-        });
-        return (json.result);
-    }
+    });
+    return (json.result);
+}
